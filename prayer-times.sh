b9814619466606553f83f8f809c1bffb9010a46a
Ainun Najib <ainunnajib@gmail.com>
2015-05-12 08:27:21 +0800

    prayer times by city

---
+++ prayer-times.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cityname='gresik'
+jsonURL=`echo http://muslimsalat.com/$cityname.json`
+channelname='#general'
+username=`echo $cityname-prayer-times`
+emojicon=':dromedary_camel:'
+webhookURL='https://hooks.slack.com/services/<your slack webhook cryptic secrets>'
+
+json=`curl -s -X GET $jsonURL`
+prayertimes=`echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w 'fajr\|shurooq\|dhuhr\|asr\|maghrib\|isha'`
+textmessage=`echo payload={\"channel\": \"$channelname\", \"username\": \"$username\", \"text\": \"[$prayertimes\", \"icon_emoji\": \"$emojicon\"}`
+curl -X POST --data-urlencode "$textmessage" $webhookURL
+
