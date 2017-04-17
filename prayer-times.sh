4d62e23f456706df84b9e417a46582c58ba2b135
AncientDeer <hakammalik00@gmail.com>
2017-04-16 21:31:49 -0600

    Update prayer-times.sh

--- prayer-times.sh
+++ prayer-times.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cityname='gresik'
+cityname='Edmonton'
 jsonURL=`echo http://muslimsalat.com/$cityname.json`
 channelname='#general'
 username=`echo $cityname-prayer-times`
 emojicon=':dromedary_camel:'
-webhookURL='https://hooks.slack.com/services/<your slack webhook cryptic secrets>'
+webhookURL='https://hooks.slack.com/services/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'
 
 json=`curl -s -X GET $jsonURL`
 prayertimes=`echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w 'fajr\|shurooq\|dhuhr\|asr\|maghrib\|isha'`
